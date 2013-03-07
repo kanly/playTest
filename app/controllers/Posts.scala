493538839401e607bffbf09743edd409dc3a7eda
Paolo Mazzoncini <p.mazzoncini@gmail.com>
2013-03-07 17:02:11 +0100

    Add detail window

--- app/controllers/Posts.scala
+++ app/controllers/Posts.scala
@@ -4,11 +4,17 @@ import play.api.mvc._
 import models.Post
 
 object Posts extends Controller {
+
+  val posts=(List(Post("titolo", "body", 12), Post("titolo2", "body2", 13)) map { t:Post => (t.id, t)}).toMap
+
+
   def list(userName: String, category: String = "all") = Action {
-    Ok(views.html.post.list(List(Post("titolo", "body", 12), Post("titolo2", "body2", 13))))
+    Ok(views.html.post.list(posts map (_._2) toList))
   }
 
-  def post(postId: Long) = TODO
+  def post(postId: Long) = Action {
+    Ok(views.html.post.detail(posts(postId)))
+  }
 
   def comments(postId: Long) = TODO
 }
